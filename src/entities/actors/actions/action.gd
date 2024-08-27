6784a45e8faf69e35e5faacdc59b914f4c4a9148
Cédric LESUEUR <clesueur@sise-plastics.com>
2024-08-27 16:30:10 +0200

    Initial commit (Part 4)

---
+++ src/entities/actors/actions/action.gd
@@ -0,0 +1,5 @@
+class_name Action
+extends RefCounted
+
+func perform(_game: Game, _entity: Entity) -> void:
+	pass
---
+++ src/entities/actors/actions/escape_action.gd
@@ -0,0 +1,5 @@
+class_name EscapeAction
+extends Action
+
+func perform(game: Game, _entity: Entity) -> void:
+	game.get_tree().quit()
